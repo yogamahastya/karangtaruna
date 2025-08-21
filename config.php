6c4d0ae7418739f7b22d45508d136836443ad04e
yogamahastya <mahastyayoga97@gmail.com>
2025-08-21 23:32:19 +0700

    update admin/index.php

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// File: config.php
+
+// Konfigurasi Nama Organisasi
+define('ORGANIZATION_NAME', 'Karang Taruna Apa Aja Lah');
+
+// Konfigurasi Koneksi Database
+define('DB_SERVERNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'karangtaruna_db');
+
+// Buat koneksi mysqli
+$conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// **START MODIFIKASI: Tambahkan definisi jabatan**
+define('JABATAN_OPTIONS', [
+    'Anggota',
+    'Ketua',
+    'Wakil Ketua',
+    'Bendahara',
+    'Sekretaris',
+    'Humas'
+]);
+// **END MODIFIKASI**
+?>
