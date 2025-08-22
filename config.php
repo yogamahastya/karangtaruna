f28a5a232eb46aeb768949743458981f2103d681
yogamahastya <mahastyayoga97@gmail.com>
2025-08-22 13:26:17 +0700

    update config

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 
 // Konfigurasi Nama Organisasi
 define('ORGANIZATION_NAME', 'Karang Taruna Apa Aja Lah');
-
+// Ganti dengan jumlah iuran bulanan yang sebenarnya
+define('DUES_MONTHLY_FEE', 10000); 
 // Konfigurasi Koneksi Database
 define('DB_SERVERNAME', 'localhost');
 define('DB_USERNAME', 'root');
