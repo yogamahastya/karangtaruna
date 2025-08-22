a3e23503e81466e08578bc1dbf2919d401d3b8c0
yogamahastya <mahastyayoga97@gmail.com>
2025-08-22 13:41:59 +0700

    update config fee

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 // File: config.php
-
 // Konfigurasi Nama Organisasi
 define('ORGANIZATION_NAME', 'Karang Taruna Apa Aja Lah');
 // Ganti dengan jumlah iuran bulanan yang sebenarnya
@@ -10,15 +9,12 @@ define('DB_SERVERNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'karangtaruna_db');
-
 // Buat koneksi mysqli
 $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
 // Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
 // **START MODIFIKASI: Tambahkan definisi jabatan**
 define('JABATAN_OPTIONS', [
     'Anggota',
